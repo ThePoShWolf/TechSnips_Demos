0ddb12f6b0b5920d0543d343bd8e17dda47048cf
Anthony Howell <anthony@howell-it.com>
2018-05-07 10:34:41 -0700

    Pulled the commands out of the string variable as well.

--- ExecutePowerShellString.ps1
+++ ExecutePowerShellString.ps1
@@ -9,11 +9,20 @@ $multiLineScriptBlock = $null
 #endregion
 
 #region single line
+Get-Process PowerShell | Format-Table Name,Id
 $singleLine = "Get-Process PowerShell | Format-Table Name,Id"
 & $singleLine
 #endregion
 
+Clear-Host
+
 #region multi line
+Get-Process PowerShell
+If($proc.Count){
+    Write-Output "There are $($proc.count) PowerShell consoles running!"
+}Else{
+    Write-Output "There is only one PowerShell console running!"
+}
 $multiLine = @'
 $proc = Get-Process PowerShell
 If($proc.Count){
