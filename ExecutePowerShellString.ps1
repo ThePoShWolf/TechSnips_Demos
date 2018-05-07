02f18361adf707fdd54b4ac3f15830d18544a023
Anthony Howell <anthony@howell-it.com>
2018-05-07 09:21:12 -0700

    And the contents

--- ExecutePowerShellString.ps1
+++ ExecutePowerShellString.ps1
@@ -1,12 +1,30 @@
+#region demo
+Throw "This is a demo, dummy!"
+#endregion
+
+#region clean
+Function Prompt(){}
+Clear-Host
+$singleLine = $multiLine = $null
+$multiLineScriptBlock = $null
+#endregion
+
+#region single line
 $singleLine = "Get-Process"
-#& $singleLine
+& $singleLine
+#endregion
+
+#region multi line
 $multiLine = @'
 $proc = Get-Process PowerShell
 If($proc.Count){
     Write-Output "There are $($proc.count) PowerShell consoles running!"
 }
 '@
+& $multiLine
 
-#& $multiLine
+#region spoiler
 $multiLineScriptBlock = [scriptblock]::Create($multiLine)
 Invoke-Command -ScriptBlock $multiLineScriptBlock
+#endregion
+#endregion
