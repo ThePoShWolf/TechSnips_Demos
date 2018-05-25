e7d136e2e69c7098e60e94f65b4a8b0da93e4f0a
Anthony Howell <anthony@howell-it.com>
2018-05-25 13:06:02 -0700

    draft one for autoreply

--- Exchange/Exchange_AutoReply.ps1
+++ Exchange/Exchange_AutoReply.ps1
@@ -3,13 +3,67 @@ Throw 'This is a demo, dummy!'
 #endregion
 
 #region prep
-
+. 'C:\Program Files\Microsoft\Exchange Server\V15\bin\RemoteExchange.ps1'
+Connect-ExchangeServer -auto -ClientApplication:ManagementShell
 #endregion
 
 #region clean
-
+Function Prompt(){}
+Clear-Host
 #endregion
 
 #region demo
+
+Get-PSSession
+
+$Enabled = @{
+    'Identity' = ''
+    'AutoReplyState' = 'Enabled'
+    'ExternalAudience' = 'All'
+    'InternalMessage' = 'I am currently out of the office binge watching TechSnips.io.'
+    'ExternalMessage' = 'I am currently out of the office'
+}
+
+Set-MailboxAutoReplyConfiguration @Enabled
+
+Get-MailboxAutoReplyConfiguration $Enabled.Identity
+
+$MessageHTML = @"
+<html>
+    <head>
+        <style type="text/css" style="display:none">
+            <!--
+                p
+                {margin-top:0;
+                margin-bottom:0}
+            -->
+        </style>
+    </head>
+    <body dir="ltr">
+        <div id="divtagdefaultwrapper" dir="ltr" style="font-size:12pt; color:#000000; font-family:Calibri,Helvetica,sans-serif">
+            <p>Hello,</p>
+            <p><br></p>
+            <p>Thanks for reaching out! I will be out of the office binge watching snips on TechSnips.io. Please reach out to my manager until I finish.</p>
+            <p><br></p>
+            <p>Thanks,</p>
+            <p>Anthony<br></p>
+        </div>
+    </body>
+</html>
+"@
+
+$Scheduled = @{
+    'Identity' = ''
+    'AutoReplyState' = 'Scheduled'
+    'StartTime' = (Get-Date).AddDays(1)
+    'EndTime' = (Get-Date).AddDays(7)
+    'InternalMessage' = $MessageHTML
+    'ExternalAudience' = 'Known'
+    'ExternalMessage' = $MessageHTML
+}
+
+Set-MailboxAutoReplyConfiguration @Scheduled
+
+Get-MailboxAutoReplyConfiguration $Scheduled.Identity
 
 #endregion
