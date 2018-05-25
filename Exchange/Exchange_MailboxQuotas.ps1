43063fdc8e0210f77ae4c204f5671fb1ac34f54a
Anthony Howell <anthony@howell-it.com>
2018-05-25 14:28:13 -0700

    scaffolded the base of each demo

--- Exchange/Exchange_MailboxQuotas.ps1
+++ Exchange/Exchange_MailboxQuotas.ps1
@@ -3,11 +3,13 @@ Throw 'This is a demo, dummy!'
 #endregion
 
 #region prep
-
+. 'C:\Program Files\Microsoft\Exchange Server\V15\bin\RemoteExchange.ps1'
+Connect-ExchangeServer -auto -ClientApplication:ManagementShell
 #endregion
 
 #region clean
-
+Function Prompt(){}
+Clear-Host
 #endregion
 
 #region demo
--- Exchange/Exchange_SendAs.ps1
+++ Exchange/Exchange_SendAs.ps1
@@ -3,7 +3,8 @@ Throw 'This is a demo, dummy!'
 #endregion
 
 #region prep
-
+. 'C:\Program Files\Microsoft\Exchange Server\V15\bin\RemoteExchange.ps1'
+Connect-ExchangeServer -auto -ClientApplication:ManagementShell
 #endregion
 
 #region clean
