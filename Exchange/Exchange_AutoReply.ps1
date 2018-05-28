38c5e7ddcd0d2b9a7a944109b2294e97238f5e08
Anthony Howell <anthony@howell-it.com>
2018-05-28 11:23:23 -0700

    Added and reorganized

--- Exchange/Exchange_AutoReply.ps1
+++ Exchange/Exchange_AutoReply.ps1
@@ -10,24 +10,36 @@ Connect-ExchangeServer -auto -ClientApplication:ManagementShell
 #region clean
 Function Prompt(){}
 Clear-Host
+Set-MailboxAutoReplyConfiguration -Identity 'Maurice Moss' -AutoReplyState Disabled -InternalMessage '' -ExternalMessage ''
 #endregion
 
 #region demo
 
 Get-PSSession
 
+#region active
+
 $Enabled = @{
-    'Identity' = ''
+    'Identity' = 'Maurice Moss'
     'AutoReplyState' = 'Enabled'
-    'ExternalAudience' = 'All'
-    'InternalMessage' = 'I am currently out of the office binge watching TechSnips.io.'
-    'ExternalMessage' = 'I am currently out of the office'
+    'ExternalAudience' = 'All' #'None', 'All', 'Known'
+    'InternalMessage' = 'I am currently out of the office binge watching TechSnips.io. Please contact Jen Barber for assistance.'
+    'ExternalMessage' = 'I am currently out of the office.'
 }
 
+#Set auto reply config
 Set-MailboxAutoReplyConfiguration @Enabled
 
-Get-MailboxAutoReplyConfiguration $Enabled.Identity
+#Get auto reply config
+Get-MailboxAutoReplyConfiguration $Enabled.Identity | Select-Object Identity,AutoReplyState,ExternalAudience,InternalMessage,ExternalMessage | Format-List
 
+Send-MailMessage -To 'MauriceMoss@techsnipsdemo.org' -From 'sysah@techsnipsdemo.org' -Subject 'This is a demo message 1' -SmtpServer ex01.techsnipsdemo.org
+
+#endregion
+
+#region scheduled
+
+#create HTML formatted message
 $MessageHTML = @"
 <html>
     <head>
@@ -41,29 +53,35 @@ $MessageHTML = @"
     </head>
     <body dir="ltr">
         <div id="divtagdefaultwrapper" dir="ltr" style="font-size:12pt; color:#000000; font-family:Calibri,Helvetica,sans-serif">
-            <p>Hello,</p>
+            <p><strong>Hello!</strong></p>
             <p><br></p>
-            <p>Thanks for reaching out! I will be out of the office binge watching snips on TechSnips.io. Please reach out to my manager until I finish.</p>
+            <p>I am out of the office binge watching snips on <a href="https://techsnips.io">TechSnips.io</a>. Please reach out to Jen Barber until I finish.</p>
             <p><br></p>
             <p>Thanks,</p>
-            <p>Anthony<br></p>
+            <p><em>Maurice</em><br></p>
         </div>
     </body>
 </html>
 "@
 
 $Scheduled = @{
-    'Identity' = ''
+    'Identity' = 'Maurice Moss'
     'AutoReplyState' = 'Scheduled'
-    'StartTime' = (Get-Date).AddDays(1)
+    'StartTime' = (Get-Date)
     'EndTime' = (Get-Date).AddDays(7)
     'InternalMessage' = $MessageHTML
     'ExternalAudience' = 'Known'
     'ExternalMessage' = $MessageHTML
 }
 
+#Set auto reply config
 Set-MailboxAutoReplyConfiguration @Scheduled
 
-Get-MailboxAutoReplyConfiguration $Scheduled.Identity
+#Get auto reply config
+Get-MailboxAutoReplyConfiguration $Scheduled.Identity | Select-Object Identity,AutoReplyState,ExternalAudience,InternalMessage,ExternalMessage,StartTime,EndTime | Format-List
+
+Send-MailMessage -To 'MauriceMoss@techsnipsdemo.org' -From 'sysah@techsnipsdemo.org' -Subject 'This is a demo message 2' -SmtpServer ex01.techsnipsdemo.org
+
+#endregion
 
 #endregion
