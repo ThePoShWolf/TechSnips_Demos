f7613a1a774ced865a3f98437e5571f76ae8503e
Anthony Howell <anthony@howell-it.com>
2018-05-30 14:13:04 -0700

    added in all commands for recording

--- Exchange/Exchange_MailboxPermission.ps1
+++ Exchange/Exchange_MailboxPermission.ps1
@@ -8,16 +8,29 @@ Connect-ExchangeServer -auto -ClientApplication:ManagementShell
 #endregion
 
 #region clean
+Remove-MailboxPermission -Identity "Jack O'Neill" -User 'George Hammond' -AccessRights FullAccess -Confirm:$false
 Function Prompt(){}
 Clear-Host
 #endregion
 
 #region demo
 
-#get current
+Get-PSSession
 
-#add full access, read only
+#Get current permissions
+Get-MailboxPermission -Identity "Jack O'Neill" | Where-Object IsInherited -eq $false
 
-#remove access
+#Add full access
+Add-MailboxPermission -Identity "Jack O'Neill" -User 'George Hammond' -AccessRights FullAccess
+
+Get-MailboxPermission -Identity "Jack O'Neill" | Where-Object IsInherited -eq $false
+
+#Possible permissions: FullAccess, SendAs, ExternalAccount, DeleteItem, ReadPermission, ChangePermission, ChangeOwner
+
+#Remove access
+Remove-MailboxPermission -Identity "Jack O'Neill" -User 'George Hammond' -AccessRights FullAccess
+
+#Add read permission
+Add-MailboxPermission -Identity "Jack O'Neill" -User 'George Hammond' -AccessRights ReadPermission
 
 #endregion
