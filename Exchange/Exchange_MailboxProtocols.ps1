5c88fea809071f860e268513a17000f391ccd7c8
Anthony Howell <anthony@howell-it.com>
2018-05-30 11:04:36 -0700

    Added code for snip

--- Exchange/Exchange_MailboxProtocols.ps1
+++ Exchange/Exchange_MailboxProtocols.ps1
@@ -8,16 +8,38 @@ Connect-ExchangeServer -auto -ClientApplication:ManagementShell
 #endregion
 
 #region clean
+$CASMailbox = @{
+    'Identity' = 'Jerry Smith'
+    'ActiveSynceEnabled' = $true
+    'PopEnabled' = $true
+    'ImapEnabled' = $true
+    'MapiEnabled' = $true
+    'OWAEnabled' = $true
+}
+Get-Mailbox | Set-CASMailbox -OWAEnabled $true
 Function Prompt(){}
 Clear-Host
 #endregion
 
 #region demo
 
-#get
+Get-PSSession
 
-#set
+#Get current protocol settings
+Get-CASMailbox -Identity 'Jerry Smith'
 
-#mass set
+#Set a mailbox's settings
+$CASMailbox = @{
+    'Identity' = 'Jerry Smith'
+    'ActiveSynceEnabled' = $false
+    'PopEnabled' = $false
+    'ImapEnabled' = $false
+}
+Set-CASMailbox -Identity 'Jerry Smith'
+
+#Set all mailboxes
+Get-Mailbox | Set-CASMailbox -OWAEnabled $false
+
+Get-CASMailbox | Group-Object OWAEnabled
 
 #endregion
