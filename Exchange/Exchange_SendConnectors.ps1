776e262c6a42215f3936e8b7fd6bf03048e5763f
Anthony Howell <anthony@howell-it.com>
2018-05-30 10:30:50 -0700

    Added new files for new snips, scaffolded out the regions

---
+++ Exchange/Exchange_SendConnectors.ps1
@@ -0,0 +1,16 @@
+#region demo header
+Throw 'This is a demo, dummy!'
+#endregion
+
+#region prep
+. 'C:\Program Files\Microsoft\Exchange Server\V15\bin\RemoteExchange.ps1'
+Connect-ExchangeServer -auto -ClientApplication:ManagementShell
+#endregion
+
+#region clean
+
+#endregion
+
+#region demo
+
+#endregion
