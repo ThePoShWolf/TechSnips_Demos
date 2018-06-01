c2b579ae29174610245349eb435a16a5b7aee4f6
Anthony Howell <anthony@howell-it.com>
2018-06-01 14:03:25 -0700

    Added code

--- Exchange/Exchange_MoveMailbox.ps1
+++ Exchange/Exchange_MoveMailbox.ps1
@@ -8,18 +8,34 @@ Connect-ExchangeServer -auto -ClientApplication:ManagementShell
 #endregion
 
 #region clean
+Remove-MoveRequest -Identity 'Anthony Howell' -Confirm:$false
+If((Get-Mailbox -Identity 'Anthony Howell').Database -eq 'TechSnipsDemoDB'){
+    New-MoveRequest 'Anthony Howell' -TargetDatabase 'Mailbox Database 1384644361'
+    While((Get-MoveRequest -Identity 'anthony howell').Status -ne 'Completed'){
+        Start-Sleep -Seconds 2
+    }
+}
 Function Prompt(){}
 Clear-Host
 #endregion
 
 #region demo
 
-#get current db
+Get-PSSession
 
-#move
+#Get the current DB of the mailbox
+Get-Mailbox -Identity 'Anthony Howell' | Select-Object -Property Database
 
-#check status
+#Create a move request
+New-MoveRequest -Identity 'Anthony Howell' -TargetDatabase TechSnipsDemoDB
 
-#get new db
+#Check the status of the move request
+Get-MoveRequest -Identity 'Anthony Howell'
+
+#Remove when finished
+Remove-MoveRequest -Identity 'Anthony Howell' -Confirm:$false
+
+#Verify the new DB
+Get-Mailbox -Identity 'Anthony Howell' | Select-Object -Property Database
 
 #endregion
