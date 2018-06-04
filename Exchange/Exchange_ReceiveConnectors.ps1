4e678ddbb4d92b4efa0c0d843d96941d29d22c23
Anthony Howell <anthony@howell-it.com>
2018-06-04 13:21:43 -0700

    Initial commit for receive connectors

---
+++ Exchange/Exchange_ReceiveConnectors.ps1
@@ -0,0 +1,23 @@
+#region demo header
+Throw 'This is a demo, dummy!'
+#endregion
+
+#region prep
+. 'C:\Program Files\Microsoft\Exchange Server\V15\bin\RemoteExchange.ps1'
+Connect-ExchangeServer -auto -ClientApplication:ManagementShell
+#endregion
+
+#region clean
+Function Prompt(){}
+Clear-Host
+#endregion
+
+#region demo
+
+#get
+
+#set
+
+#remove
+
+#endregion
