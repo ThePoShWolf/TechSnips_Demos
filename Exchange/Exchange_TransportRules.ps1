a62c5caae5f32f5f285c7374ca46b565e088ed22
Anthony Howell <anthony@howell-it.com>
2018-06-04 15:05:36 -0700

    Final edits, added code

--- Exchange/Exchange_TransportRules.ps1
+++ Exchange/Exchange_TransportRules.ps1
@@ -5,19 +5,97 @@ Throw 'This is a demo, dummy!'
 #region prep
 . 'C:\Program Files\Microsoft\Exchange Server\V15\bin\RemoteExchange.ps1'
 Connect-ExchangeServer -auto -ClientApplication:ManagementShell
+New-Mailbox 'Hubert Farnsworth' -UserPrincipalName HubertFarnsworth@techsnipsdemo.org -Password (ConvertTo-SecureString 'Password1!' -AsPlainText -Force)
+New-DistributionGroup 'Accounting'
 #endregion
 
 #region clean
+Get-TransportRule | Remove-TransportRule -Confirm:$false
 Function Prompt(){}
 Clear-Host
 #endregion
 
 #region demo
 
-#get
+Get-PSSession
 
-#set
+#region Creating transport rules
+
+#https://docs.microsoft.com/en-us/powershell/module/exchange/policy-and-compliance/New-TransportRule?view=exchange-ps
+
+#region Tag inbound emails
+$TransportRule = @{
+    'Name' = 'Tag External'
+    'FromScope' = 'NotInOrganization'
+    'PrependSubject' = '[External] '
+}
+New-TransportRule @TransportRule
+
+Get-TransportRule $TransportRule.Name | Select-Object -Property Description | Format-List
+
+#endregion
+
+#region Disclaim outbound emails
+$TransportRule = @{
+    'Name' = 'Disclaimer'
+    'FromScope' = 'InOrganization'
+    'SentToScope' = 'NotInOrganization'
+    'ApplyHtmlDisclaimerText' = 'Forwarding this message is punishable by 10 lashes per forwarded recipient.'
+    'ApplyHtmlDisclaimerLocation' = 'Append' #Prepend
+    'ApplyHtmlDisclaimerFallbackAction' = 'Wrap' #Ignore, Reject
+}
+New-TransportRule @TransportRule
+
+Get-TransportRule $TransportRule.Name | Select-Object -Property Description | Format-List
+
+#endregion
+
+#region CC Accounting with exceptions
+$TransportRule = @{
+    'Name' = 'CC Accounting on invoices and POs'
+    'FromScope' = 'NotInOrganization'
+    'SentToScope' = 'InOrganization'
+    'SubjectContainsWords' = 'Invoice','Purchase Order'
+    'CopyTo' = 'Accounting@TechSnipsDemo.org'
+    'ExceptIfSentToMemberOf' = 'Accounting' #Distribution Group or Security Group
+    'ExceptIfMessageTypeMatches' = 'Calendaring'
+    #OOF,AutoForward,Encrypted,PermissionControlled,Voicemail,Signed,ApprovalRequest,ReadReceipt
+}
+New-TransportRule @TransportRule
+
+Get-TransportRule $TransportRule.Name | Select-Object -Property Description | Format-List
+
+#endregion
+
+#endregion
+
+#region Managing transport rules
+
+#https://docs.microsoft.com/en-us/powershell/module/exchange/policy-and-compliance/set-transportrule?view=exchange-ps
+
+#Set Priority
+Get-TransportRule
+
+#Set disclaimer to top priority
+Set-TransportRule -Identity 'Disclaimer' -Priority 0
+
+Get-TransportRule
+
+#Set tag external to except the IT Department
+Set-TransportRule -Identity 'Tag External' -ExceptIfSentToMemberOf 'IT Department'
+
+#Disable/Enable
+Disable-TransportRule -Identity 'CC Accounting on Invoices and POs' -Confirm:$false
+
+Get-TransportRule
+
+Enable-TransportRule -Identity 'CC Accounting on Invoices and POs'
 
 #remove
+Remove-TransportRule -Identity 'Disclaimer' -Confirm:$false
+
+Get-TransportRule
+
+#endregion
 
 #endregion
