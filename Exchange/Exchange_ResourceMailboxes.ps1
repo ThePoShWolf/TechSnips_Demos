6640bfa0621d4abedfda9c0a3eec356b5a5dbac7
Anthony Howell <anthony@howell-it.com>
2018-06-05 15:14:03 -0700

    Initial script for recording

--- Exchange/Exchange_ResourceMailboxes.ps1
+++ Exchange/Exchange_ResourceMailboxes.ps1
@@ -8,18 +8,68 @@ Connect-ExchangeServer -auto -ClientApplication:ManagementShell
 #endregion
 
 #region clean
+Remove-Mailbox 'Ship' -Confirm:$false
+Remove-Mailbox 'Smell-O-Scope' -Confirm:$false
 Function Prompt(){}
 Clear-Host
 #endregion
 
 #region demo
 
-#create
+#region Create resource mailboxes
 
-#manage settings
+#Create a room mailbox
+$RoomMailbox = @{
+    'Room' = $true
+    'Name' = 'Ship'
+    'DisplayName' = 'Planet Express Ship'
+}
+New-Mailbox @RoomMailbox
 
-#calendar processing
+Get-ADUser -Identity 'Ship'
+
+#Create an equipment mailbox
+$EquipmentMailbox = @{
+    'Equipment' = $true
+    'Name' = 'Smell-O-Scope'
+    'DisplayName' = 'Smell-O-Scope'
+}
+New-Mailbox @EquipmentMailbox
+
+Get-ADUser -Identity 'Smell-O-Scope'
+
+#endregion
+
+#region Set calendar processing
+
+#Set it on our room
+$RoomCalendarProcessing = @{
+    'Identity' = 'Ship@techsnipsdemo.org'
+    'AutomateProcessing' = 'AutoAccept'
+    'BookingWindowInDays' = 365
+    'DeleteAttachments' = $false
+}
+Set-CalendarProcessing @RoomCalendarProcessing
+
+Get-CalendarProcessing 'Ship'
+
+#Set it on our equipment
+$EquipmentCalendarProcessing = @{
+    'Identity' = 'Smell-O-Scope@techsnipsdemo.org'
+    'AutomateProcessing' = 'None'
+    'ForwardRequestsToDelegates' = $true
+    'ResourceDelegates' = 'Hubert Farnsworth'
+    'BookingWindowInDays' = 30
+    'MaximumDurationInMinutes' = 15
+    'AllowRecurringMeetings' = $false
+}
+Set-CalendarProcessing @EquipmentCalendarProcessing
+
+Get-CalendarProcessing 'Smell-O-Scope'
+
+#endregion
 
 #removing
+Remove-Mailbox -Identity 'Ship' -Confirm:$false
 
 #endregion
--- Exchange/Exchange_UserMailboxes.ps1
+++ Exchange/Exchange_UserMailboxes.ps1
@@ -8,16 +8,100 @@ Connect-ExchangeServer -auto -ClientApplication:ManagementShell
 #endregion
 
 #region clean
+Remove-Mailbox 'BenderRodriguez@techsnipsdemo.org' -Confirm:$false
+Disable-Mailbox 'Philip J. Fry' -Confirm:$false
 Function Prompt(){}
 Clear-Host
 #endregion
 
 #region demo
 
-#get
+Get-PSSession
 
-#create (enable, new)
+#region Create a mailbox for an existing AD user
+Get-ADUser -Identity 'Philip J. Fry' -Properties EmailAddress | Format-Table Name,EmailAddress
 
-#remove (disable, remove)
+#Create the mailbox in Exchange
+Enable-Mailbox -Identity 'Philip J. Fry' -Database 'TechSnipsDemoDB'
+
+#Look at the email address property in AD
+Get-ADUser -Identity 'Philip J. Fry' -Properties EmailAddress | Format-Table Name,EmailAddress
+
+#Look at the shiny new mailbox
+Get-Mailbox -Identity 'Philip J. Fry'
+
+#endregion
+
+#region Create a mailbox without an existing AD user
+$Mailbox = @{
+    'Password' = ConvertTo-SecureString 'P@ssword1!' -AsPlainText -Force
+    'Name' = 'Bender Rodriguez'
+    'UserPrincipalName' = 'BenderRodriguez@techsnipsdemo.org'
+    'FirstName' = 'Bender'
+    'LastName' = 'Rodriguez'
+    'Database' = 'TechSnipsDemoDB'
+    'ResetPasswordOnNextLogon' = $true
+}
+New-Mailbox @Mailbox
+
+#Check Exchange
+Get-Mailbox -Identity 'BenderRodriguez@techsnipsdemo.org'
+
+#Check AD
+Get-ADUser -Identity 'BenderRodriguez'
+
+#endregion
+
+#region Manage SMTP addresses
+Get-Mailbox -Identity 'BenderRodriguez' | Format-List EmailAddresses
+
+#Set the email address to a single SMTP address
+$Bender = @{
+    'Identity' = 'BenderRodriguez'
+    'EmailAddressPolicyEnabled' = $false
+    'EmailAddresses' = 'Bender@ProjectScissorsDemo.org'
+}
+Set-Mailbox -Identity 'BenderRodriguez' -EmailAddressPolicyEnabled $false -EmailAddresses 'BenderR@ProjectScissorsDemo.org'
+
+#Check the new address
+Get-Mailbox -Identity 'BenderRodriguez' | Format-List EmailAddresses
+
+#Add and remove email addresses, specifying a primary SMTP address
+$NewBender = @{
+    'Identity' = 'BenderRodriguez'
+    'EmailAddressPolicyEnabled' = $false
+    'EmailAddresses' = @{
+        Add='SMTP:Bender@TechSnipsDemo.org','Bender@ProjectScissorsDemo.org';
+        Remove='BenderR@ProjectScissorsDemo.org'
+    }
+}
+Set-Mailbox @NewBender
+
+#Check the new addresses
+Get-Mailbox -Identity 'BenderRodriguez' | Format-List EmailAddresses
+
+#endregion
+
+#region Disable a mailbox
+Disable-Mailbox -Identity 'Philip J. Fry' -Confirm:$false
+
+#Check AD
+Get-ADUser -Identity 'Philip J. Fry'
+
+#Check Exchange
+Get-Mailbox -Identity 'Philip J. Fry'
+
+#endregion
+
+#region Remove a mailbox
+Remove-Mailbox -Identity 'BenderRodriguez' -Confirm:$false
+
+#Check AD
+Get-ADUser -Identity 'BenderRodriguez'
+
+#Check Exchange
+Get-Mailbox -Identity 'BenderRodriguez'
+
+#endregion
 
 #endregion
