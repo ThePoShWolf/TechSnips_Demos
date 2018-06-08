64545766ba97e0ba8cbc096d78ea673aaa450b6c
Anthony Howell <anthony@howell-it.com>
2018-06-08 11:09:41 -0700

    basic stuff

--- Exchange/Exchange_ReceiveConnectors.ps1
+++ Exchange/Exchange_ReceiveConnectors.ps1
@@ -8,13 +8,28 @@ Connect-ExchangeServer -auto -ClientApplication:ManagementShell
 #endregion
 
 #region clean
+Remove-ReceiveConnector 'Executive MFP' -Confirm:$false
 Function Prompt(){}
 Clear-Host
 #endregion
 
 #region demo
 
-#get
+#Get all receive connectors
+Get-ReceiveConnector
+
+#New receive connector for SMTP gateway
+
+#New receive connector for internal devices
+$ReceiveConnector = @{
+    'Name' = 'Executive MFP'
+    'Custom' = $true
+    'Bindings' = '0.0.0.0:25'
+    'RemoteIPRanges' = '10.2.2.42'
+    'TransportRole' = 'Frontend'
+    'PermissionGroups' = 'AnonymousUsers'
+}
+New-ReceiveConnector @ReceiveConnector
 
 #set
 
