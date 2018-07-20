59c57c9b0619b3b33438cbf54ab762b627d96b7b
Anthony Howell <anthony@howell-it.com>
2018-07-20 15:10:29 -0700

    Final, as recorded

--- Misc/NTFSPermissions.ps1
+++ Misc/NTFSPermissions.ps1
@@ -3,12 +3,22 @@ Throw "This is a demo, dummy!"
 #endregion
 
 #region clean
-Function Prompt(){}
-Clear-Host
-Remove-Item $dir
+If(Test-Path $dir){
+    Remove-Item $dir -Recurse -Force -Confirm:$false
+}
 New-Item $dir -ItemType Directory
 New-Item "$dir\dog.txt" -ItemType File
 New-Item "$dir\cat.txt" -ItemType File
+#add fido to dog.txt
+$identity = 'techsnipsdemo\Fido'
+$rights = 'FullControl'
+$type = 'Allow'
+$ACE = New-Object System.Security.AccessControl.FileSystemAccessRule($identity,$rights,$type)
+$ACL = Get-Acl "$dir\dog.txt"
+$ACL.AddAccessRule($ACE)
+Set-Acl "$dir\dog.txt" -AclObject $ACL
+Function Prompt(){}
+Clear-Host
 #endregion
 
 #region variables
@@ -30,22 +40,38 @@ Get-Acl $dir | Select-Object -ExpandProperty Access
 
 #Additional useful stuff
 #filtering for a user or group
-(Get-Acl $dir).Access | Where-Object {$_.IdentityReference -like '*admin*'} | Format-Table
+(Get-Acl $dir).Access | Where-Object {$_.IdentityReference -like '*sysah'} | Format-Table
 
 #Finding uninherited permissions
-(Get-Acl $dir).Access | Where-Object {-not ($_.IsInherited)} | Format-Table
+(Get-Acl $dir).Access | Where-Object {-not $_.IsInherited} | Format-Table
 
 #endregion
 
 #region Setting Acls
-#Microsoft example. Boring.
+#current ACLs
+(Get-ACL "$dir\dog.txt").Access | Format-Table
+(Get-ACL "$dir\cat.txt").Access | Format-Table
+
+#Copy dog to cat
 $ACL = Get-Acl "$dir\Dog.txt"
 Set-Acl -Path "$dir\Cat.txt" -AclObject $ACL
+
+#Check cat
+(Get-ACL "$dir\cat.txt").Access | Format-Table
 
 #region Add an ACE to an existing ACL on a folder
 #Create the ACE
 $identity = 'techsnipsdemo\Fido'
 $rights = 'FullControl' #other options: [enum]::GetValues('System.Security.AccessControl.FileSystemRights')
+<#
+Inheritance is what types child objects the ACE applies to. With a filesystem, containers = folders, objects = files.
+
+Propagation controls which generation of child objects the ACE is restricted to. None = ACE applies to all.
+InheritOnly = ACE applies only to children and grandchildren, not to target folder. NoPropagateInherit = Target folder
+and target folder children, not grandchildren.
+
+https://msdn.microsoft.com/en-us/library/ms229747(v=vs.110).aspx
+#>
 $inheritance = 'ContainerInherit, ObjectInherit' #other options: [enum]::GetValues('System.Security.AccessControl.InheritanceFlags')
 $propagation = 'None' #other options: [enum]::GetValues('System.Security.AccessControl.PropagationFlags')
 $type = 'Allow' #other options: [enum]::GetValues('System.Security.AccessControl.AccessControlType')
@@ -57,20 +83,27 @@ $ACL.AddAccessRule($ACE)
 
 #Set-Acl
 Set-Acl $dir -AclObject $ACL
+
+#Verify
+(Get-ACL $dir).Access | Format-Table
 #endregion
 
-#region Remove an ACE on an existing ACL on a folder
+#region Remove an ACE on an existing ACL on a file
 #Get existing ACL
-$ACL = Get-Acl $dir
+(Get-Acl "$dir\cat.txt").Access | Format-Table
+$ACL = Get-Acl "$dir\cat.txt"
 
 #Filter for the ACE to remove
-$ACE = $ACL.Access | ?{$_.IdentityReference -eq 'techsnipsdemo\Fido'}
+$ACE = $ACL.Access | Where-Object {($_.IdentityReference -eq 'techsnipsdemo\Fido') -and -not ($_.IsInherited)}
 
 #Remove the ACE
 $ACL.RemoveAccessRule($ACE)
 
 #Set the ACL
-Set-Acl $dir -AclObject $ACL
+Set-Acl "$dir\cat.txt" -AclObject $ACL
+
+#Verify
+(Get-Acl "$dir\cat.txt").Access | Format-Table
 #endregion
 
 #endregion
