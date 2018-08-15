5e5ba33c0bbcbb97f55059fef58315b93af2ddc2
Anthony Howell <anthony@howell-it.com>
2018-08-15 15:49:36 -0700

    Added some spacing, corrected some notes

--- Active Directory/PowerShell_BulkEditADObjects.ps1
+++ Active Directory/PowerShell_BulkEditADObjects.ps1
@@ -13,9 +13,9 @@ $OUPath = 'OU=People,DC=techsnips,DC=local'
 'Chiefs','Minions' | %{New-ADOrganizationalUnit $_ -Path $OUPath}
 
 #users
-'Roy','Maurice','Jen','Richmond' | %{New-ADUser $_ -Department 'IT' -Company 'Reynholm Industries' -UserPrincipalName "$_.techsnips.local"}
+'Roy','Maurice','Jen','Richmond' | %{New-ADUser $_ -Department 'IT' -Company 'Reynholm Industries' -UserPrincipalName "$_`@techsnips.local"}
 'Roy','Maurice' | %{Set-ADUser $_ -Title 'Service Desk Tech'}
-'Douglas Reynholm','Denholm Reynholm' | %{New-ADUser $_ -Department 'Chiefs' -Company 'Reynholm Industries' -UserPrincipalName "$_.techsnips.local"}
+'Douglas Reynholm','Denholm Reynholm' | %{New-ADUser $_ -Department 'Chiefs' -Company 'Reynholm Industries' -UserPrincipalName "$_`@techsnips.local"}
 Set-ADuser 'Jen' -Title 'Relationship Manager'
 'Douglas Reynholm','Denholm Reynholm' | %{Set-ADUser $_ -Title 'Chief Executive Officer'}
 'Douglas Reynholm','Denholm Reynholm' | %{Set-ADAccountPassword $_ -Reset -NewPassword (ConvertTo-SecureString 'Something!Long@And#Random$' -AsPlainText -Force)}
@@ -25,11 +25,12 @@ Set-ADuser 'Jen' -Title 'Relationship Manager'
 'Roy','Maurice','Jen','Richmond','Douglas Reynholm' | %{Set-ADUser $_ -Enabled $true}
 
 #groups
-'IT Department','Chiefs Department',"Minions of Jen" | %{New-ADGroup $_ -GroupScope Universal -GroupCategory Security}
+'IT Department','Chiefs Department','Service Desk' | %{New-ADGroup $_ -GroupScope Universal -GroupCategory Security}
 
 #clean
 'Roy','Maurice','Jen','Richmond','Douglas Reynholm','Denholm Reynholm' | %{Remove-ADUser $_ -Confirm:$false}
-'IT Department','Chiefs Department','Minions of Jen' | %{Remove-ADGroup $_ -Confirm:$false}
+'IT Department','Chiefs Department','Service Desk' | %{Remove-ADGroup $_ -Confirm:$false}
+"OU=Chiefs,$OUPath","OU=Minions,$OUPath" | %{Set-ADOrganizationalUnit $_ -ProtectedFromAccidentalDeletion $False}
 "OU=Chiefs,$OUPath","OU=Minions,$OUPath" | %{Remove-ADOrganizationalUnit $_ -Confirm:$false}
 #endregion
 
@@ -37,19 +38,25 @@ Set-ADuser 'Jen' -Title 'Relationship Manager'
 #Filtering for all users
 Get-ADUser -Filter * | Format-Table Name
 
+
 #Filtering by a single attribute
 Get-ADUser -Filter {Department -eq "IT"} | Format-Table Name
+
 Get-ADUser -Filter {Title -like "*Manager"} | Format-Table Name
+
 
 #Filtering by multiple attributes
 Get-ADUser -Filter {(Department -eq "IT") -and (Manager -eq "Jen")} | Format-Table Name
+
 $before = (Get-Date).AddDays(-2)
 $after = (Get-Date).AddDays(-6)
 Get-ADUser -Filter {(Created -gt $after) -and (Created -lt $before)} | Format-Table Name
 
+
 #Retrieving users by OU
 $OUPath = 'OU=People,DC=techsnips,DC=local'
 Get-ADUser -Filter * -SearchBase $OUPath | Format-Table Name
+
 
 #Both
 Get-ADUSer -SearchBase $OUPath -Filter {(Enabled -eq $true) -and (PasswordExpired -eq $false)} | Format-Table Name
@@ -60,17 +67,19 @@ Get-ADUSer -SearchBase $OUPath -Filter {(Enabled -eq $true) -and (PasswordExpire
 
 #Update UserPrincipalName (i.e. for O365)
 #Pipeline, ForEach-Object
-Get-ADUser -SearchBase $OUPath -Filter {UserPrincipalName -notlike "*techsnips.io"}
+Get-ADUser -SearchBase $OUPath -Filter {UserPrincipalName -notlike "*techsnips.io"} | Format-Table Name,UserPrincipalName
 
 Get-ADUser -Filter {UserPrincipalName -notlike "*techsnips.io"} | `
 %{Set-ADUser $_ -UserPrincipalName "$($_.SamAccountName)`@techsnips.io"}
 
-Get-ADUser -Filter {UserPrincipalName -notlike "*techsnips.io"}
+Get-ADUser -SearchBase $OUPath -Filter {UserPrincipalName -notlike "*techsnips.io"} | Format-Table Name,UserPrincipalName
+
 
 #Foreach
 ForEach($user in Get-ADUser -Filter {UserPrincipalName -notlike "*techsnips.io"}){
     Set-ADUser -Identity $user -UserPrincipalName "$($user.SamAccountName)`@techsnips.io"
 }
+
 
 #Reset password expiration date for C Levels
 Get-ADUser -Filter {Enabled -eq $true} -SearchBase "OU=Chiefs,$OUPath" -Properties PasswordLastSet | Format-Table Name,PasswordLastSet
@@ -83,25 +92,35 @@ ForEach($user in Get-ADUser -Filter {Enabled -eq $true} -SearchBase "OU=Chiefs,$
 }
 Get-ADUser -Filter {Enabled -eq $true} -SearchBase "OU=Chiefs,$OUPath" -Properties PasswordLastSet | Format-Table Name,PasswordLastSet
 
+
 #Add users by title into groups
 Get-ADGroupMember 'Service Desk' | Format-Table Name
+
 ForEach($user in Get-ADUser -Filter {Title -eq 'Service Desk Tech'}){
     Write-Host "Adding $($user.name) to group: Service Desk"
     Add-ADGroupMember 'Service Desk' -Members $user.SamAccountName
 }
 Get-ADGroupMember 'Service Desk' | Format-Table Name
 
+
 #Make sure each user is a member of their department group
 #$user.Department = Human Resources
 #Group = Human Resources Department
+
 Get-ADGroupMember 'IT Department' | Format-Table Name
 Get-ADGroupMember 'Chiefs Department' | Format-Table Name
+
+$user = Get-ADUser -Identity 'Roy' -Properties MemberOf
+$user.MemberOf
+$user.MemberOf -Replace '(CN=)*(,.*)*'
+
 ForEach($user in Get-ADUser -Filter {Enabled -eq $true} -SearchBase $OUPath -Properties Department,MemberOf){
     If($user.MemberOf -replace '(CN=)*(,.*)*' -notcontains "$($user.Department) Department"){
         Write-Host "Adding $($user.name) to group: $($user.Department) Department"
         Add-ADGroupMember "$($user.Department) Department" -Members $user.SamAccountName
     }
 }
+
 Get-ADGroupMember 'IT Department' | Format-Table Name
 Get-ADGroupMember 'Chiefs Department' | Format-Table Name
 #endregion
