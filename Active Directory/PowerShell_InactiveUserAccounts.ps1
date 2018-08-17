0d383d8f8b21d67682c9da8a98560993452a23f9
Anthony Howell <anthony@howell-it.com>
2018-08-17 15:18:41 -0700

    First run at adding in code

--- Active Directory/PowerShell_InactiveUserAccounts.ps1
+++ Active Directory/PowerShell_InactiveUserAccounts.ps1
@@ -7,3 +7,41 @@ Function Prompt(){}
 Clear-Host
 #endregion
 
+#region Search-ADAccount
+
+#By time span
+Search-ADAccount -AccountInactive -TimeSpan '30.00:00:00'
+
+Search-ADAccount -AccountInactive -TimeSpan (New-TimeSpan -Days 30)
+
+#By date
+Search-ADAccount -AccountInactive -DateTime "08/10/2018 00:00:00"
+
+Search-ADAccount -AccountInactive -DateTime (Get-Date).AddDays(-30)
+
+#Removing the users
+Search-ADAccount -AccountInactive -TimeSpan '30.00:00:00' -UsersOnly |`
+Remove-ADUser -WhatIf
+
+#endregion
+
+#region Avoiding newly created accounts
+
+#Using search base
+Search-ADAccount -AccountInactive -DateTime (Get-Date).AddDays(-10) -SearchBase ''
+
+#Or using Get-ADUser -filter
+$date = (Get-Date).AddDays(-10)
+Get-ADUser -Filter {LastLogonDate -lt $date}
+Get-ADUser -Filter {LastLogonDate -notlike "*"}
+Get-ADUser -Filter {Created -lt $date}
+
+#All together
+$filter = {
+    ((LastLogonDate -lt $date) -or (LastLogonDate -notlike "*"))
+    -and (Created -lt $date)
+}
+
+Get-ADUser -Filter $filter | Remove-ADUser -WhatIf
+
+#endregion
