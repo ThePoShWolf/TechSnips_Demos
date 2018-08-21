04679619bb5e4cb091d38e2616ee5df92dd61fe0
Anthony Howell <anthony@howell-it.com>
2018-08-21 16:25:00 -0700

    Finished initial additions

--- Active Directory/PowerShell_ADComputers.ps1
+++ Active Directory/PowerShell_ADComputers.ps1
@@ -30,13 +30,49 @@ New-ADComputer -Instance $templateComp -Name 'Sierra'
 #endregion
 
 #region Get-ADComputer
+#Get one computer
+Get-ADComputer -Identity 'Sierra'
+
+#Filter for all computers
+Get-ADComputer -Filter *
+
+#Filter for all Server 2016
+Get-ADComputer -Filter {OperatingSystem -like '*2016*' }
+
+#Get additional properties
+Get-ADComputer -Identity 'Sierra' -Properties *
+
+Get-ADComputer -Identity 'Sierra' -Properties IPv4Address
+
+#Get computers from a specific OU
+Get-ADComputer -Filter * -SearchBase 'OU=Super Computers,OU=Machines,DC=techsnips,DC=local'
 
 #endregion
 
 #region Set-ADComputer
 
+#Certain properties
+Set-ADComputer -Identity 'Summit' -Enabled $False
+
+$SetCompSplat = @{
+    Identity = 'Summit'
+    DisplayName = "DoE's finest."
+    Location = 'Oak Ridge National Laboratory'
+}
+Set-ADComputer @SetCompSplat
+
+#Properties without explicit parameter
+$Comp = Get-ADComputer -Identity 'Summit'
+$Comp.PrimaryGroup = ''
+
 #endregion
 
 #region Remove-ADComputer
+
+Remove-ADComputer -Identity 'Sierra'
+
+$20mAgo = (Get-Date).AddMinutes(-20)
+Get-ADComputer -Filter {Created -gt $20mAgo}
+Get-ADComputer -Filter {Created -gt $20mAgo} | Remove-ADComputer -Confirm:$false
 
 #endregion
