881812253d3e43a141242c75d7b4ac3b5503be9e
Anthony Howell <anthony@howell-it.com>
2018-08-21 12:04:00 -0700

    Added more code, plus the get-gpallregistryvalues function

--- Active Directory/PowerShell_RegistrySettingsGPO.ps1
+++ Active Directory/PowerShell_RegistrySettingsGPO.ps1
@@ -6,3 +6,41 @@ Throw "This is a demo, dummy!"
 Function Prompt(){}
 Clear-Host
 #endregion
+
+#region Basic how-to
+
+$GPOName = 'Default Domain Policy'
+
+Get-GPRegistryValue -Name $GPOName -Key 'HKLM\Software'
+
+Get-GPRegistryValue -Name $GPOName -Key 'HKLM\Software\Policies'
+
+Get-GPRegistryValue -Name $GPOName -Key 'HKLM\Software\Policies\Microsoft\SystemCertificates\EFS'
+
+#endregion
+
+#region Recursive function
+#https://github.com/gpoguy/ADMXToDSC/blob/master/ADMXToDSC.PS1
+Function Get-GPAllRegistryValues{
+    [cmdletbinding()]
+    param(
+        [string]$key,
+        [string]$gpoName
+    )
+    $current = Get-GPRegistryValue -Name $gpo -Key $key -ErrorAction SilentlyContinue
+    If($current){
+        Foreach($item in $current){
+            If ($item.ValueName){
+                $item
+            }Else{
+                Get-GPAllRegistryValues -Key $item.FullKeyPath -gpoName $gpo
+            }
+        }
+    }
+}
+#endregion
+
+$BaseKeys = 'HKCU\Software','HKLM\System','HKLM\Software'
+$GPOs = Get-GPO -All
+
+ForEach($GPO in $GPOs)
