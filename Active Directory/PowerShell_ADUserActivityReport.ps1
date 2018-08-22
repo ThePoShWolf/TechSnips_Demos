b636cbc0f1574e0fcb17a68e6cc62ab9705d37ac
Anthony Howell <anthony@howell-it.com>
2018-08-22 13:46:46 -0700

    Final edits, including fixing the pipeline input for the final function

--- Active Directory/PowerShell_ADUserActivityReport.ps1
+++ Active Directory/PowerShell_ADUserActivityReport.ps1
@@ -7,7 +7,7 @@ Function Prompt(){}
 Clear-Host
 #endregion
 
-#region Getting started
+#region Getting the info we need
 #Event IDs to look for
 #4648 is explicit logon, 4647 is logoff
 $EventIds = 4647,4648
@@ -44,15 +44,18 @@ $Events[0].ID
 #Account
 ($Events | Where-Object ID -ne $Events[0].ID)[0].Properties[1].Value
 
+#Return an object
 ForEach($event in $Events){
     Switch($event.ID){
         4647 {
             $Account = $event.Properties[1].Value
             $Domain = $event.Properties[2].Value
+            $Type = 'Logoff'
         }
         4648 {
             $Account = $event.Properties[5].Value
             $Domain = $event.Properties[6].Value
+            $Type = 'Logon'
         }
     }
     [PSCustomObject]@{
@@ -60,28 +63,26 @@ ForEach($event in $Events){
         Time = $event.TimeCreated
         Account = $Account
         Domain = $Domain
+        Type = $Type
     }
 }
 
 #endregion
 
-
-
-#Region Bring it together with a function
+#region Bring it together with a function
 Function Get-ADUserActivityReport{
     Param(
         [Parameter(
-            Mandatory = $true,
             ValueFromPipelineByPropertyName = $true
         )]
-        [Alias('Name')]
-        [string]$ComputerName
+        [Alias('ComputerName')]
+        [string]$Name
     )
     Begin{
         $EventIDs = 4647,4648
     }
     Process{
-        $events = Get-WinEvent -ComputerName $ComputerName -FilterHashtable {
+        $events = Get-WinEvent -ComputerName $Name -FilterHashtable @{
             LogName = 'Security'
             ID = $EventIDs
         } 
@@ -90,24 +91,33 @@ Function Get-ADUserActivityReport{
                 4647 {
                     $Account = $event.Properties[1].Value
                     $Domain = $event.Properties[2].Value
+                    $Type = 'Logoff'
                 }
                 4648 {
                     $Account = $event.Properties[5].Value
                     $Domain = $event.Properties[6].Value
+                    $Type = 'Logon'
                 }
             }
             [PSCustomObject]@{
-                ComputerName = $ComputerName
+                ComputerName = $Name
                 Time = $event.TimeCreated
                 Account = $Account
                 Domain = $Domain
+                Type = $Type
             }
         }
     }
     End{}
 }
 
-Get-ADUserActivityReport -ComputerName 'Prod-DC'
+Get-ADUserActivityReport -ComputerName 'Prod-DC' | Format-Table
 
-Get-ADComputer -Filter * -SearchBase
+Get-ADComputer -Filter * | Get-ADUserActivityReport | Format-Table
+
+$ExportPath = 'C:\Users\techsnips\Desktop\ADUserReport.csv'
+Get-ADComputer -Filter * | Get-ADUserActivityReport | Export-CSV $ExportPath -NoTypeInformation
+
+. $ExportPath
+
 #endregion
