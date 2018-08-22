274bf4795192b0e3e1af7cdfc99c9125c97748cb
Anthony Howell <anthony@howell-it.com>
2018-08-21 17:13:42 -0700

    Added more regions and added validations

--- Active Directory/PowerShell_ADComputers.ps1
+++ Active Directory/PowerShell_ADComputers.ps1
@@ -11,7 +11,9 @@ Clear-Host
 #simplest
 New-ADComputer 'Summit'
 
-#Set more properties
+Get-ADComputer -Identity 'Summit'
+
+#region Set more properties
 $NewCompSplat01 = @{
     Name = 'Sunway TaihuLight'
     SamAccountName = 'Sunway.TaihuLight'
@@ -20,33 +22,60 @@ $NewCompSplat01 = @{
 }
 New-ADComputer @NewCompSplat01
 
-#Copy from template computer
+Get-ADComputer -Identity $NewCompSplat01.SamAccountName
+#endregion
+
+#region Copy from template computer
 $GetCompSplat = @{
     Identity = 'Sunway.TaihuLight'
     Properties = 'Location','OperatingSystem','OperatingSystemHotfix','OperatingSystemServicePack','OperatingSystemVersion'
 }
 $templateComp = Get-ADComputer @GetCompSplat
 New-ADComputer -Instance $templateComp -Name 'Sierra'
+
+Get-ADcomputer -Identity 'Sierra'
+#endregion
+#endregion
+
+#region Move
+
+#Move computer
+(Get-ADComputer -Identity 'Summit').DistinguishedName
+
+$MoveCompSplat = @{
+    Identity = (Get-ADComputer -Identity 'Summit').DistinguishedName
+    TargetPath = 'OU=Super Computers,OU=Machines,DC=techsnips,DC=local'
+}
+Move-ADObject @MoveCompSplat
+
+(Get-ADComputer -Identity 'Summit').DistinguishedName
 #endregion
 
 #region Get-ADComputer
-#Get one computer
-Get-ADComputer -Identity 'Sierra'
-
 #Filter for all computers
 Get-ADComputer -Filter *
 
-#Filter for all Server 2016
-Get-ADComputer -Filter {OperatingSystem -like '*2016*' }
+#region Filter for all Server 2016
+Get-ADComputer -Filter {OperatingSystem -like '*2016*'}
 
-#Get additional properties
+Get-ADComputer -Filter {Enabled -eq $false}
+#endregion
+
+#region Get additional properties
 Get-ADComputer -Identity 'Sierra' -Properties *
 
 Get-ADComputer -Identity 'Sierra' -Properties IPv4Address
+#endregion
 
-#Get computers from a specific OU
+#region Group memberships
+Add-ADGroupMember -Identity 'IT' -Members (Get-ADComputer -Identity 'Sierra').SID
+
+(Get-ADComputer -Identity 'Sierra' -Properties MemberOf).Memberof
+#endregion
+
+#region Get computers from a specific OU
 Get-ADComputer -Filter * -SearchBase 'OU=Super Computers,OU=Machines,DC=techsnips,DC=local'
-
+#endregion
 #endregion
 
 #region Set-ADComputer
@@ -54,25 +83,36 @@ Get-ADComputer -Filter * -SearchBase 'OU=Super Computers,OU=Machines,DC=techsnip
 #Certain properties
 Set-ADComputer -Identity 'Summit' -Enabled $False
 
+Get-ADComputer -Identity 'Summit'
+
+#region Multiple properties
 $SetCompSplat = @{
     Identity = 'Summit'
-    DisplayName = "DoE's finest."
+    Description = "DoE's finest."
     Location = 'Oak Ridge National Laboratory'
 }
 Set-ADComputer @SetCompSplat
 
-#Properties without explicit parameter
-$Comp = Get-ADComputer -Identity 'Summit'
-$Comp.PrimaryGroup = ''
+Get-ADComputer -Identity 'Summit' -Properties Description,Location
+#endregion
 
+#region Properties without explicit parameter
+$Comp = Get-ADComputer -Identity 'Summit' -Properties ProtectedFromAccidentalDeletion
+$Comp.ProtectedFromAccidentalDeletion = $false
+Set-ADComputer -Instance $Comp
+
+Get-ADComputer $Comp -Properties ProtectedFromAccidentalDeletion
+#endregion
 #endregion
 
 #region Remove-ADComputer
 
+#One computer
 Remove-ADComputer -Identity 'Sierra'
 
+#region Filtered computers
 $20mAgo = (Get-Date).AddMinutes(-20)
 Get-ADComputer -Filter {Created -gt $20mAgo}
 Get-ADComputer -Filter {Created -gt $20mAgo} | Remove-ADComputer -Confirm:$false
-
+#endregion
 #endregion
