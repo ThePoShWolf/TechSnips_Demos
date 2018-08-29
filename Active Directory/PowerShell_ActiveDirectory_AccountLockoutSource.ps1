9261df84b397af6aad4306f401d770485e1b28a0
Anthony Howell <anthony@howell-it.com>
2018-08-29 14:11:39 -0700

    Initial add during stream

--- Active Directory/PowerShell_ActiveDirectory_AccountLockoutSource.ps1
+++ Active Directory/PowerShell_ActiveDirectory_AccountLockoutSource.ps1
@@ -7,4 +7,20 @@ Function Prompt(){}
 Clear-Host
 #endregion
 
+#region
+
+#Account lockout Event ID
+$LockOutID = 4740
+
+#Find the PDC
+(Get-ADDomain).PDCEmulator
+$PDCEmulator = (Get-ADDomain).PDCEmulator
+
+#Query event log
+Get-WinEvent -ComputerName $PDCEmulator -FilterHashtable @{
+    LogName = 'Security'
+    ID = $LockOutID
+}
+
+
 #endregion
