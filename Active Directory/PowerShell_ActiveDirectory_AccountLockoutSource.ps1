f04b81e998012dc77ea7f85637b837cb3dcf9914
Anthony Howell <anthony@howell-it.com>
2018-08-29 11:24:18 -0700

    starting from scratch to stream

--- Active Directory/PowerShell_ActiveDirectory_AccountLockoutSource.ps1
+++ Active Directory/PowerShell_ActiveDirectory_AccountLockoutSource.ps1
@@ -7,18 +7,4 @@ Function Prompt(){}
 Clear-Host
 #endregion
 
-#region Find the information
-#Lock out event ID
-$LockoutID = 4740
-
-#Lockouts will always happen on the PDC
-(Get-ADDomain).PDCEmulator
-
-#So all we have to do is to query the PDC logs?
-$PDCEmulator = (Get-ADDomain).PDCEmulator
-Get-WinEvent -ComputerName $PDCEmulator -FilterHashtable @{
-    LogName = 'Security'
-    ID = $LockoutID
-}
-
 #endregion
