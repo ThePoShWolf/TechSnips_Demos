ba260f21fc800abf3ddf0a15facd647e681ed9e3
Anthony Howell <anthony@howell-it.com>
2018-12-05 14:28:19 -0800

    More hyper v stuff

---
+++ Courses/Learning Windows Server 2019/LoadTemplate.ps1
@@ -0,0 +1,20 @@
+﻿$newVMName = 'Test VM'
+$stagingVHDPath = 'C:\users\Public\Documents\Hyper-V\Virtual hard disks\New\'
+$defaultVHDPath = 'C:\users\Public\Documents\Hyper-V\Virtual hard disks'
+
+# Import the template
+$newVM = Import-VM -Path 'E:\VMTemplate\Template\Virtual Machines\C30F0BFA-2D6A-48E4-BF93-8D8057332C15.vmcx' -Copy -VhdDestinationPath $stagingVHDPath -GenerateNewId
+
+# Get the VHD Info
+$vmHD = Get-VMHardDiskDrive -VM $newVM -ControllerLocation 0 -ControllerNumber 0
+
+# Rename and move the VHD
+$newVHDPath = "$defaultVHDPath\$newVMName.vhdx"
+Move-Item $vmHD.Path -Destination $newVHDPath
+
+# Remove and attach the VHD
+Remove-VMHardDiskDrive -VMHardDiskDrive $vmHD
+Add-VMHardDiskDrive -VM $newVM -ControllerType SCSI -ControllerNumber 0 -ControllerLocation 0 -Path $newVHDPath
+
+# Rename VM
+Rename-VM -VM $newVM -NewName $newVMName
---
+++ Courses/Learning Windows Server 2019/New-VMFromTemplate.ps1
@@ -0,0 +1,28 @@
+﻿Function New-VMFromTemplate {
+    Param(
+        [string]$NewVMName,
+        [string]$TemplatePath = 'E:\VMTemplate\Template',
+        [string]$StagingVHDPath = 'C:\users\Public\Documents\Hyper-V\Virtual hard disks\New\',
+        [string]$DefaultVHDPath = 'C:\users\Public\Documents\Hyper-V\Virtual hard disks'
+    )
+    $TemplateFile = Get-Item (Join-Path $TemplatePath -ChildPath 'Virtual Machines\*.vmcx')
+    If(Test-Path $TemplateFile.FullName){
+
+        # Import the template
+        $newVM = Import-VM -Path $TemplateFile.FullName -Copy -VhdDestinationPath $stagingVHDPath -GenerateNewId
+
+        # Get the VHD Info
+        $vmHD = Get-VMHardDiskDrive -VM $newVM -ControllerLocation 0 -ControllerNumber 0
+
+        # Rename and move the VHD
+        $newVHDPath = "$defaultVHDPath\$newVMName.vhdx"
+        Move-Item $vmHD.Path -Destination $newVHDPath
+
+        # Remove and attach the VHD
+        Remove-VMHardDiskDrive -VMHardDiskDrive $vmHD
+        Add-VMHardDiskDrive -VM $newVM -ControllerType SCSI -ControllerNumber 0 -ControllerLocation 0 -Path $newVHDPath
+
+        # Rename VM
+        Rename-VM -VM $newVM -NewName $newVMName
+    }
+}
