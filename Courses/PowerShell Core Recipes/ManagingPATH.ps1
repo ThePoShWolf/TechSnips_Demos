33781a1affaf6ad2d9b93915adda04bf62aae7e9
Anthony Howell <anthony@howell-it.com>
2018-12-13 14:54:29 -0800

    last minute update

--- Courses/PowerShell Core Recipes/ManagingPATH.ps1
+++ Courses/PowerShell Core Recipes/ManagingPATH.ps1
@@ -4,10 +4,11 @@ $env:Path
 # Updating
 $env:Path += ';C:\tmp'
 
-# Demonstration
+#region Demonstration
 executeme.ps1
 
 New-Item C:\tmp\executeme.ps1 -Value "Write-Host 'Hello from executeme.ps1'"
 
 Get-Location
 executeme.ps1
+#endregion
