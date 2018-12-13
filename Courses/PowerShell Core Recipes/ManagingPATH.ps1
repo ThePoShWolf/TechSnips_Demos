78f0adccc27085597a71a2788072f7698e8baf2d
Anthony Howell <anthony@howell-it.com>
2018-12-13 15:09:51 -0800

    Adding removal options as well

--- Courses/PowerShell Core Recipes/ManagingPATH.ps1
+++ Courses/PowerShell Core Recipes/ManagingPATH.ps1
@@ -1,14 +1,27 @@
 # Path location
 $env:Path
 
-# Updating
+$env:Path.Split(';')
+
+#region Adding
 $env:Path += ';C:\tmp'
 
-#region Demonstration
+# Demonstration
 executeme.ps1
 
 New-Item C:\tmp\executeme.ps1 -Value "Write-Host 'Hello from executeme.ps1'"
 
 Get-Location
 executeme.ps1
+#endregion
+
+#region Removing
+$env:Path.Split(';').Count
+
+$env:Path = $env:path -replace "\;C\:\\tmp$",''
+
+$env:Path = $env:Path -replace [regex]::Escape(';C:\Program Files (x86)\Common Files\Oracle\Java\javapath'),''
+
+$env:Path.Split(';').Count
+
 #endregion
