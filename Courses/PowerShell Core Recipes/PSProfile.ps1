de7be4cbf44558d127f7dd329881b215b38173c9
Anthony Howell <anthony@howell-it.com>
2018-12-14 14:41:38 -0800

    Added some API stuff for kicks

--- Courses/PowerShell Core Recipes/PSProfile.ps1
+++ Courses/PowerShell Core Recipes/PSProfile.ps1
@@ -1,4 +1,12 @@
 # Windows
+# Location: $profile
+#region Prep
+$profile
+$profile = 'C:\Users\Anthony\Documents\PowerShell\Microsoft.PowerShell_profile.ps1'
+If(-not(Test-Path $profile)){
+    New-Item $profile -ItemType File
+}
+#endregion
 Function prompt {
     $prompt = 'pwsh '
     $prompt += "[$($PSVersionTable.PSVersion)] "
@@ -17,13 +25,29 @@ Function prompt {
 
 $env:Path += ';C:\tmp'
 
+$cityID = '5725846'
+$weatherAPIKey = Get-Content C:\users\Anthony\Documents\OpenWeatherAPI.txt
+$url = "https://api.openweathermap.org/data/2.5/weather?id=$cityID&APPID=$weatherAPIKey"
+$resp = Invoke-RestMethod -Uri $url -Method Get
+
 Write-Host "Welcome $($env:USERNAME), it is $(Get-Date -Format 'HH:mm:ss')"
+Write-Host "The weather is currently: $($resp.weather.description)"
 
 Function Go-Home{
     Set-Location $env:USERPROFILE
 }
 
-# Linux
+# Linux and macOS
+# Location: ~/.config/powershell/profile.ps1
+#region Prep
+$profilePath = '~/.config/powershell/profile.ps1'
+If(-not(Test-Path $profilePath)){
+    If(-not(Test-Path (Split-Path $profilePath))){
+        New-Item (Split-Path $profilePath) -ItemType Directory
+    }
+    New-Item $profilePath -ItemType File
+}
+#endregion
 Function prompt {
     $prompt = 'pwsh '
     $prompt += "[$($PSVersionTable.PSVersion)] "
@@ -41,8 +65,15 @@ Function prompt {
 }
 
 $env:Path += ':/usr/tmp'
+$cityID = '5725846'
+$weatherAPIKey = Get-Content ~/Documents/OpenWeatherAPI.txt
+$url = "https://api.openweathermap.org/data/2.5/weather?id=$cityID&APPID=$weatherAPIKey"
+$resp = Invoke-RestMethod -Uri $url -Method Get
 
 Write-Host "Welcome $($env:USER), it is $(Get-Date -Format 'HH:mm:ss')"
+Write-Host "The weather is currently: $($resp.weather.description)"
+
+
 
 Function Go-Home{
     Set-Location ~
