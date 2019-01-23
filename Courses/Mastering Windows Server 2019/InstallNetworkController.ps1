fadec7878bf84baa0037f71bfda3a944a822613c
Anthony Howell <anthony@howell-it.com>
2019-01-23 15:40:57 -0800

    Initial start

---
+++ Courses/Mastering Windows Server 2019/InstallNetworkController.ps1
@@ -0,0 +1,23 @@
+#region prep
+$cred = Get-Credential
+$net01 = New-PSSession Net01 -Credential $cred
+$dc01 = New-PSSession DC01 -Credential $cred
+#endregion
+
+#region Prepare to deploy
+# Create Security group
+Invoke-Command -Session $dc01 -ScriptBlock {
+    New-ADGroup -GroupCategory Security -GroupScope Global -Name 'Network Controller Clients'
+    Add-ADGroupMember 'Network Controller Clients' -Members 'DC01$','DC02$'
+}
+
+Enter-PSSession -Session $net01
+
+# Install the role
+Install-WindowsFeature -Name NetworkController -IncludeManagementTools
+
+#endregion
+
+$node = New-NetworkControllerNodeObject -Name 'Net01' -Server 'Net01' -FaultDomain 'Fd:/Lab1/Rack1/Host1' -RestInterface 'ethernet'
+
+Install-NetworkController -Node $node -ClientAuthentication Kerberos -ClientSecurityGroup 'Network Controller Clients'
