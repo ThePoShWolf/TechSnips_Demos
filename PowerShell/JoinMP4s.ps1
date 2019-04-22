93b2316fcca4a4c33406796f4a60a04b6e8aabb0
Anthony Howell <anthony@howell-it.com>
2019-04-22 13:15:45 -0700

    New snip!

---
+++ PowerShell/JoinMP4s.ps1
@@ -0,0 +1,88 @@
+#region demo
+Throw "This is a demo, dummy!"
+#endregion
+#region clean
+Function Prompt(){}
+Clear-Host
+#endregion
+
+# http://ffmpeg.org/download.html
+# https://trac.ffmpeg.org/wiki/Concatenate
+
+#region Prep
+# Go to the correct location
+Set-Location D:\TechSnips\Demo
+
+# Path to ffmpeg
+$ffmpeg = '.\ffmpeg\bin\ffmpeg.exe'
+
+# Path to ffprobe (for verification)
+$ffprobe = '.\ffmpeg\bin\ffprobe.exe'
+
+# Path to the files
+$file1 = '.\setupAksClusterTake2Part1.mp4'
+$file2 = '.\setupAksClusterTake2Part2.mp4'
+
+# Current status
+(& $ffprobe $file1 -v quiet -of json -show_format | ConvertFrom-Json).format
+(& $ffprobe $file2 -v quiet -of json -show_format | ConvertFrom-Json).format
+
+# Transcode them to MPEG-2 transport streams (H.264 video and AAC audio)
+& $ffmpeg -y -i $file1 -c copy -bsf:v h264_mp4toannexb -f mpegts temp1.ts
+& $ffmpeg -y -i $file2 -c copy -bsf:v h264_mp4toannexb -f mpegts temp2.ts
+
+# Temp file status
+(& $ffprobe .\temp1 -v quiet -of json -show_format | ConvertFrom-Json).format
+
+# Concat them together
+& $ffmpeg -f mpegts -i "concat:temp1.ts|temp2.ts" -c copy -bsf:a aac_adtstoasc output.mp4
+
+# Final file
+(& $ffprobe output.mp4 -v quiet -of json -show_format | ConvertFrom-Json).format
+
+#endregion
+
+#region As a function!
+Function Join-ffmpegMp4 {
+    param (
+        [Parameter(
+            ValueFromPipeline = $true
+        )]
+        [ValidateScript({
+            $_.Name -match '\.mp4'
+        })]
+        [System.IO.FileInfo[]]$Files,
+        [System.IO.DirectoryInfo]$TempFolder,
+        [System.IO.FileInfo]$OutputFile,
+        [string]$ffmpeg = 'D:\TechSnips\Demo\ffmpeg\bin\ffmpeg.exe'
+    )
+    Begin{
+        $outFiles = @()
+    }
+    Process {
+        # Create all the tmp files
+        $tmpFile = "$($TempFolder.FullName)\$($Files.BaseName).ts"
+        & $ffmpeg -y -i "$($Files.FullName)" -c copy -bsf:v h264_mp4toannexb -f mpegts $tmpFile -v quiet
+        $outFiles += $tmpFile
+    }
+    End {
+        # Join them
+        $concatString = "concat:" + ($outFiles -join '|')
+        & $ffmpeg -f mpegts -i $concatString -c copy -bsf:a aac_adtstoasc $OutputFile -v quiet
+        # Clean up
+        foreach ($file in $outFiles){
+            Remove-Item $file -Force
+        }
+    }
+}
+
+# Usage
+Join-ffmpegMp4 -Files .\setupAksClusterTake2Part1.mp4, .\setupAksClusterTake2Part2.mp4 -OutputFile .\output.mp4 -TempFolder .\
+
+Remove-Item .\output.mp4
+
+# Or use the pipeline
+Get-ChildItem -Filter *.mp4 | Join-ffmpegMp4 -OutputFile .\output.mp4 -TempFolder .\
+
+Get-Item .\output.mp4
+#endregion
